5442df3fc9310e8c51f9b898308a92d5fae164f3
Niket420 <niketanand420@gmail.com>
2026-02-27 00:25:49 +0530

    implemented logout feature in user.controllers.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -100,7 +100,28 @@ const login = asyncHandler(async(req,res)=>{
 
 });
 
-const logout = {}
+const logout = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(req.user._id,{
+        $set:{
+            refreshToken: "",
+        },
+    },
+    {
+        new:true,
+        },
+    );
+
+    const options = {
+        httpOnly:true,
+        secure:true,
+    };
+
+    return res
+        .status(200)
+        .clearCookie("accessToken",options)
+        .clearCookie("refreshToken",options)
+        .json(new APIResponse(200,{},"User logged out"));
+});
 
 const refreshAccessToken={}
 
