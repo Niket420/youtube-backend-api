f9bdd5fb96e6495065d5e8d8713812cdac4836d6
Niket420 <niketanand420@gmail.com>
2026-03-06 16:53:23 +0530

    created subscription.route

---
+++ src/routes/subscription.model.js
@@ -0,0 +1,18 @@
+import {toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels} from "../controllers/subscriber.controllers.js"
+
+import verifyJWT from "../middlewares/auth.middleware.js"
+import {Router} from 'express'
+import upload from "../middlewares/multer.middleware.js"
+
+
+const router = Router()
+
+
+router.use(verifyJWT)
+
+router.route("/c/:channelId").post(toggleSubscription)
+                            .get(getUserChannelSubscribers)
+
+router.route("/u/:channelId").get(getSubscribedChannels)
