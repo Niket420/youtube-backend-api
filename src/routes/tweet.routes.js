c87e4f6c9c0a34b9266d199408baf7c3db24b3cf
Niket420 <niketanand420@gmail.com>
2026-03-06 16:45:51 +0530

    created tweet.routes.js

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,21 @@
+import {createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet} from "../controllers/tweet.controllers.js"
+
+import verifyJWT from "../middlewares/auth.middleware.js"
+import {Router} from 'express'
+import upload from "../middlewares/multer.middleware.js"
+
+
+const router = Router()
+
+
+router.use(verifyJWT)
+
+router.route("/").post(createTweet)
+router.route("/user/:userId").get(getUserTweets)
+router.route(":/tweetId").patch(updateTweet)
+                            .delete(deleteTweet)
+
+export default router
