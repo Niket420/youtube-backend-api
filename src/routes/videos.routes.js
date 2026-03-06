538225389099404bae78669324fd2fecda4ec79b
Niket420 <niketanand420@gmail.com>
2026-03-06 16:33:14 +0530

    just added middleware to verifyJWT in vidoes.routes.js

--- src/routes/videos.routes.js
+++ src/routes/videos.routes.js
@@ -9,6 +9,7 @@ import verifyJWT from "../middlewares/auth.middleware.js"
 import upload from "../middlewares/multer.middleware.js"
 
 const router = Router()
+router.use(verifyJWT); 
 
 
 router.route("/").get(getAllVideos)
