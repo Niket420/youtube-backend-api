f7a26ba68b3a1f8974029dfbc36cc95132e130c6
Niket420 <niketanand420@gmail.com>
2026-03-06 23:43:00 +0530

    completed app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,42 @@
-import express from 'express'
-import cors from 'cors'
-
-
-
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
-const port = process.env.PORT
 
-app.use(cors())
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 
+//routes import
+import userRouter from './routes/users.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/videos.routes.js"
+import commentRouter from "./routes/comments.routes.js"
+import likeRouter from "./routes/likes.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
+//routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
-export {app};
+// http://localhost:8000/api/v1/users/register
+
+export { app }
